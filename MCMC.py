e6e583b31e979e37118aa44af6d61e52e5dd6559
Bob Carpenter <bcarpenter@flatironinstitute.org>
2022-06-01 17:30:53 -0400

    draft of HMC sampler

--- MCMC.py
+++ MCMC.py
@@ -1,18 +1,6 @@
 import numpy as np
 
 class Metropolis:
-    _model = []
-    _dim = []
-    _q_rng = []
-    _theta = []
-    _log_p_theta = []
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return self.sample()
-
     def __init__(self, model, proposal_rng, init = []):
         self._model = model
         self._dim = self._model.dims()
@@ -20,13 +8,52 @@ class Metropolis:
         self._theta = init or np.random.normal(size = self._dim)
         self._log_p_theta = self._model.log_density(self._theta)
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.sample()
+
     def sample(self):
-        # could include logic to include initialization as first draw
+        # does not include initial value as first draw
         theta_star = self._theta + self._q_rng()
         log_p_theta_star = self._model.log_density(theta_star)
-        accept = 0
         if np.log(np.random.uniform()) < log_p_theta_star - self._log_p_theta:
             self._theta = theta_star
             self._log_p_theta = log_p_theta_star
-            accept = 1
-        return self._theta, self._log_p_theta, [accept]
+        return self._theta, self._log_p_theta
+
+    
+class HMCDiag:
+    def __init__(self, model, proposal_rng, stepsize, steps, metric_diag = [], init = []):
+        self._model = model
+        self._dim = self._model.dims()
+        self._q_rng = proposal_rng
+        self._theta = init or np.random.normal(size = self._dim)
+        self._stepsize = stepsize
+        self._steps = steps
+        self._metric = metric_diag or np.ones(self._dim)
+
+    def joint_logp(self, theta, rho):
+        return self._model.log_density(theta) - 0.5 * np.dot(rho, np.multiply(this._metric, rho))
+
+    def leapfrog(theta, rho):
+        # TODO(bob-carpenter): refactor to share non-initial and non-final updates
+        for n in range(self._steps):
+            lp, grad = self._model.log_prob_gradient(theta)
+            rho_mid = rho - 0.5 * stepsize * multiply(this._metric, grad)
+            theta = theta + stepsize * rho_mid
+            lp, grad = self._model.log_prob_gradient(theta)
+            rho = rho_mid - 0.5 * stepsize * multiply(this._metric, grad)
+        return (theta, rho)
+    
+    def sample(self):
+        theta = self._theta
+        rho = np.random.normal(size = self._dim)
+        theta_prop, rho_prop = self.leapfrog(theta, rho)
+        current_logp = self.joint_logp(theta, rho)
+        proposal_logp = self.joint_logp(theta_prop, rho_prop)
+        if np.log(np.random.uniform()) < proposal_log_prob - current_log_prob:
+            self._theta = theta_star
+        return self._theta, self._log_p_theta
+    
--- example.py
+++ example.py
@@ -1,3 +1,7 @@
+# First build stan/multi/multi with
+# > cd <stan-model-server>
+# make -j4 stan/multi/multi
+
 import StanModelClient as smc
 import MCMC as mcmc
 import numpy as np
@@ -11,6 +15,15 @@ proposal_rng = lambda : np.random.normal(size=model.dims())
 
 sampler = mcmc.Metropolis(model, proposal_rng)
 
+print(model.param_names(0, 0), ", log density, accept\n")
+for n in range(10):
+    theta = sampler.next()
+    print(theta, '\n')
+
+stepsize = 0.1
+steps = 10
+metric_diag = [1, 1]
+sampler = mcmc.HMCDiag(model, proposal_rng, stepsize, steps, metric_diag)
 print(model.param_names(0, 0), ", log density, accept\n")
 for n in range(10):
     theta = sampler.next()
