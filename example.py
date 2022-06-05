21848fcc6c27d65b55aa9fa48ee03eaaaf326f08
Bob Carpenter <carp@alias-i.com>
2022-06-05 16:14:25 -0400

    verified mean, sd for HMC

--- example.py
+++ example.py
@@ -11,24 +11,19 @@ data = "stan/multi/multi.data.json"
 
 model = smc.StanClient(server, data=data, seed=1234)
 
-stepsize = 0.1
-steps = 10
-sampler = mcmc.HMCDiag(model, stepsize=stepsize, steps=steps)
-sampler.sample()
+stepsize = 1/64
+steps = 64
+metric_diag = [1, 1]
+sampler = mcmc.HMCDiag(model, stepsize=stepsize, steps=steps, metric_diag=metric_diag)
 
-print(model.param_names(0, 0), ", log density, accept\n")
-for n in range(10):
-    theta = sampler.sample()
-    print(theta, "\n")
+# works, but excruciatingly slow
+M = 10000
+theta = np.empty([M, 2])
+for m in range(M):
+    theta[m, :], _ = sampler.sample()
+
+
 
 # proposal_rng = lambda: np.random.normal(size=model.dims())
 # sampler = mcmc.Metropolis(model, proposal_rng)
 
-stepsize = 0.1
-steps = 10
-metric_diag = [1, 1]
-sampler = mcmc.HMCDiag(model, proposal_rng, stepsize, steps, metric_diag)
-print(model.param_names(0, 0), ", log density, accept\n")
-for n in range(10):
-    theta = sampler.next()
-    print(theta, "\n")
